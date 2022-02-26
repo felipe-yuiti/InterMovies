786eb160a054c899b725c84181d91dc778a44109
Wladmir Silva <wladmir.silva@nubank.com.br>
2022-02-25 21:58:16 -0300

    Fix Header, Add SearchController

--- InterMovies/ViewController.swift
+++ InterMovies/ViewController.swift
@@ -12,9 +12,29 @@ struct HeardOnTable{
     var subTitle: String
 }
 
+class SelfSizedTableView: UITableView {
+    var maxHeight: CGFloat = UIScreen.main.bounds.size.height
+    
+    override func reloadData() {
+        super.reloadData()
+        self.invalidateIntrinsicContentSize()
+        self.layoutIfNeeded()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let height = min(contentSize.height, maxHeight)
+        return CGSize(width: contentSize.width, height: height)
+    }
+}
+
 class ViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableView: SelfSizedTableView!
+    
+    let listaDeFilmes = ["Homem Aranha", "Batmam", "Hulk"]
+    var listaDeFilmesFiltradas: [String] = []
+    
+    let searchController = UISearchController(searchResultsController: nil)
     
     let data = ["acao","aventura","drama","suspense", "terror"]
     var hearder = [HeardOnTable(title: "feijao", subTitle: "lulu"),
@@ -22,19 +42,35 @@ class ViewController: UIViewController {
                    HeardOnTable(title: "salada", subTitle: "lulu")]
     var heightOfHearder: CGFloat = 44
     
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController?.setToolbarHidden(true, animated: true)
+        
+        // tableView Setup
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.register(HearderViewTableViewCell.self, forHeaderFooterViewReuseIdentifier: "Header")
+        let nib = UINib(nibName: "HeaderView", bundle: nil)
+        tableView.register(nib, forHeaderFooterViewReuseIdentifier: "HeaderView")
+        
+        // serachBar Setup
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Pesquise seu filme..."
+        navigationItem.searchController = searchController
+        listaDeFilmesFiltradas = listaDeFilmes
+        
     }
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return listaDeFilmesFiltradas.count
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -44,17 +80,31 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         
-        let hearderView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "Header") as! HearderViewTableViewCell
+        let hearderView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "HeaderView") as! HearderViewTableViewCell
         hearderView.leftHearderLabel.text = hearder[section].title
         hearderView.rightHearderLabel.text = hearder[section].subTitle
-
+        
         return hearderView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Continuarassistindo", for: indexPath) as! MovieCell
-        cell.movieLabel.text = "Após a captura de Merry e Pippy pelos orcs, a Sociedade do Anel é dissolvida. Frodo e Sam seguem sua jornada rumo à Montanha da Perdição para destruir o anel e descobrem que estão sendo perseguidos pelo misterioso Gollum. Enquanto isso, Aragorn, o elfo e arqueiro Legolas e o anão Gimli partem para resgatar os hobbits sequestrados e chegam ao reino de Rohan, onde o rei Theoden foi vítima de uma maldição mortal de Saruman."
-        cell.timeLabel.text = "AAAAAArooo"
+        cell.movieLabel.text = listaDeFilmesFiltradas[indexPath.row]
+        cell.timeLabel.text = "Tempo restante"
         return cell
+    }
+}
+
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let filtro = searchController.searchBar.text, filtro.count > 0 {
+            listaDeFilmesFiltradas = listaDeFilmesFiltradas.filter { filme in
+                return filme.uppercased().contains(filtro.uppercased())
+            }
+        } else {
+            listaDeFilmesFiltradas = listaDeFilmes
+        }
+        
+        tableView.reloadData()
     }
 }
